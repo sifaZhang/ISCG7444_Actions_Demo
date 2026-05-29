22bd5f975d16f932c3ef85adeab1e7cf47814881
sifaz <sifazhang.nz@gmail.com>
2026-05-29 12:41:10 +1200

    As a user, I want a health endpoint (/health) so that I can check the application status.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import flask
+import json
+from flask import jsonify
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
     return 'API works!'
+
+@app.route('/health')
+def health():
+    return jsonify({'status': 'healthy'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
